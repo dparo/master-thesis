e72b45186c15021644658240865fac8bf1903d81
Davide Paro <dparo@outlook.it>
2021-10-19 11:39:35 +0200

    Started argument parsing for the main executable.

---
+++ src/version.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#if __cplusplus
+extern "C" {
+#endif
+
+extern const char *GIT_SHA1;
+extern const char *GIT_DATE;
+extern const char *GIT_COMMIT_SUBJECT;
+
+#if __cplusplus
+}
+#endif
