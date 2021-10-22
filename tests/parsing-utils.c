337dd49be526a11d6ea32dba0f1189e2d3d50945
Davide Paro <dparo@outlook.it>
2021-10-22 14:44:15 +0200

    More tests

--- tests/parsing-utils.c
+++ tests/parsing-utils.c
@@ -32,57 +32,58 @@
 #include "types.h"
 #include "parsing-utils.h"
 
+void test_parse_int32(void) {
+
 #define S(x)                                                                   \
     { #x, (int32_t)(x), true }
 #define F(x)                                                                   \
     { x, 0, false }
 
-static struct {
-    char *string;
-    int32_t expected;
-    bool expect_success;
-} i32_tests[] = {
-    // Expect success here
-    S(0),
-    S(1),
-    S(0xff),
-    S(+0xcc),
-    S(0xAB),
-    S(0b01),
-    S(-0xff),
-    S(0x7fffffff),
-    S(-0x80000000),
-    S(-0b1010101),
-    S(+0b1010101),
-    S(0b1010101),
+    static struct {
+        char *string;
+        int32_t expected;
+        bool expect_success;
+    } i32_tests[] = {
+        // Expect success here
+        S(0),
+        S(1),
+        S(0xff),
+        S(+0xcc),
+        S(0xAB),
+        S(0b01),
+        S(-0xff),
+        S(0x7fffffff),
+        S(-0x80000000),
+        S(-0b1010101),
+        S(+0b1010101),
+        S(0b1010101),
 
-    // Expect failures here
-    // Suffixes do not pass yey
-    F("0x"),
-    F("-"),
-    F("-0x"),
+        // Expect failures here
+        // Suffixes do not pass yey
+        F("0x"),
+        F("-"),
+        F("-0x"),
 
-    F("0b"),
-    F("-0b"),
-    F("-0xasd"),
-    F("-0bx10"),
-    F("-0xffu"),
-    F("-0xfful"),
-    F("-1.0"),
-    F("+1.0"),
-    // OUT of range
-    F("0xffffffff"),
-    // Text string
-    F("asdadsads"),
+        F("0b"),
+        F("-0b"),
+        F("-0xasd"),
+        F("-0bx10"),
+        F("-0xffu"),
+        F("-0xfful"),
+        F("-1.0"),
+        F("+1.0"),
+        // OUT of range
+        F("0xffffffff"),
+        // Text string
+        F("asdadsads"),
 
-    // Computations are not allowed
-    F("3 * 2"),
-};
+        // Computations are not allowed
+        F("3 * 2"),
+    };
 
 #undef S
 #undef F
 
-void test_parse_int32(void) {
     for (int32_t i = 0; i < (int32_t)ARRAY_LEN(i32_tests); i++) {
         const char *in = i32_tests[i].string;
         int32_t expected = i32_tests[i].expected;
@@ -95,9 +96,54 @@ void test_parse_int32(void) {
     }
 }
 
+void test_parse_float(void) {
+
+#define S(x)                                                                   \
+    { #x, (int32_t)(x), true }
+#define F(x)                                                                   \
+    { x, 0, false }
+
+    static struct {
+        char *string;
+        float expected;
+        bool expect_success;
+    } float_tests[] = {
+        // Successess
+
+        S(0.0), S(1.0), S(2.32), S(0.04), S(.001), S(1.), S(1e-3), S(1E-3),
+
+        S(+0.0), S(+1.0), S(+2.32), S(+0.04), S(+.001), S(+1.), S(+1e-3),
+        S(+1E-3),
+
+        S(-0.0), S(-1.0), S(-2.32), S(-0.04), S(-.001), S(-1.), S(-1e-3),
+        S(-1E-3),
+
+        S(10.0), S(10e4), S(.10), S(10.0f), S(10e4f), S(.10f),
+
+        S(-10.0), S(-10e4), S(-.10), S(-10.0f), S(-10e4f), S(-.10f),
+
+        // Failures
+        F("10f"), F("-10f")};
+
+#undef S
+#undef F
+    const float EPSILON = 0.0001;
+    for (int32_t i = 0; i < (int32_t)ARRAY_LEN(float_tests); i++) {
+        const char *in = float_tests[i].string;
+        float expected = float_tests[i].expected;
+        float obtained;
+        bool success = str_to_float(in, &obtained);
+        TEST_ASSERT(success == float_tests[i].expect_success);
+        if (success) {
+            TEST_ASSERT_FLOAT_WITHIN(EPSILON, expected, obtained);
+        }
+    }
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_parse_int32);
+    RUN_TEST(test_parse_float);
     return UNITY_END();
 }
 
