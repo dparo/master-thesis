51f2fff1b36ef8b18b2edccebc08bf552c492565
Davide Paro <dparo@outlook.it>
2021-10-22 08:51:52 +0200

    Solvers names and params available at the commandline --help message

--- src/core.c
+++ src/core.c
@@ -160,24 +160,33 @@ const char *param_type_as_str(SolverParamType type) {
 }
 
 void cptp_print_list_of_solvers_and_params(void) {
+
+    printf("\n");
+    printf("Available Solvers and Settable Params:\n");
     for (int32_t solver_idx = 0;
          solver_idx < (int32_t)ARRAY_LEN(SOLVERS_LOOKUP_TABLE); solver_idx++) {
         const SolverDescriptor *d = SOLVERS_LOOKUP_TABLE[solver_idx].descriptor;
         const char *solver_name = d->name;
         if (solver_name != NULL && *solver_name != '\0') {
-            printf("%s\n", solver_name);
+            printf("  - %s:\n", solver_name);
             for (int32_t j = 0; d->params[j].name != NULL; j++) {
                 if (d->params[j].default_value != NULL &&
                     *d->params[j].default_value != '\0') {
-                    printf("   %-20s  (%s, default: %s)\n", d->params[j].name,
+                    printf("      - %-20s  (%s, default: %s)\n",
+                           d->params[j].name,
                            param_type_as_str(d->params[j].type),
                            d->params->default_value);
                 } else {
-                    printf("   %-20s  (%s)\n", d->params[j].name,
+                    printf("      - %-20s  (%s)\n", d->params[j].name,
                            param_type_as_str(d->params[j].type));
                 }
-                printf("%32s\n", d->params->glossary);
+
+                if (d->params[j].glossary != NULL &&
+                    *d->params[j].glossary != '\0') {
+                    printf("        %s\n", d->params[j].glossary);
+                }
             }
+            printf("\n");
         }
     }
 }
