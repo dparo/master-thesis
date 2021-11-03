1a3085205b534fb1ba1a5d06fd0527ecca88b03d
Davide Paro <dparo@outlook.it>
2021-11-03 09:22:31 +0100

    Fixed bake script

--- VRPSolver/bake_cplex1210_in_docker_img.sh
+++ VRPSolver/bake_cplex1210_in_docker_img.sh
@@ -9,7 +9,7 @@ set -x
 cd "$(find ./ -mindepth 1 -maxdepth 1 -type d | grep -E 'vrpsolver.*' | sort -n | head -n 1)" || exit 1
 
 INPUT_IMG="bapdock.img.backup"
-OUTPUT_IMG="bapdock.img"
+OUTPUT_IMG="bapdock"
 
 if [ ! -f "${INPUT_IMG}" ]; then
 	cp "${OUTPUT_IMG}" "${INPUT_IMG}"
@@ -17,9 +17,9 @@ fi
 
 docker import -c 'ENTRYPOINT ["/julia"]' bapdock.img.backup bapdock-init
 docker run -it -d --name bapdock-cont bapdock-init
+container_id="$(docker ps -aqf "name=bapdock-cont")"
 docker cp /opt/ibm/ILOG/CPLEX_Studio1210/cplex bapdock-cont:/cplex
 docker cp /opt/ibm/ILOG/CPLEX_Studio1210/cplex/bin/x86-64_linux/libcplex12100.so bapdock-cont:/cplex/bin/x86-64_linux/libcplex12x.so
-container_id="$(docker ps -aqf "name=bapdock")"
 docker commit "$container_id" "${OUTPUT_IMG}"
 docker stop "${container_id}"
 docker rm "${container_id}"
