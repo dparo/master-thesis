d4098ada740ba298b5bc337e3b7f27465ef69a27
Davide Paro <dparo@outlook.it>
2021-11-24 19:10:44 +0100

    Progress

--- src/tools/perfprof/main.c
+++ src/tools/perfprof/main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#define SHARE_PROCESS_GROUP (true)
+
+pid_t spawn(char *const argv[]) {
+    pid_t pid = 0;
+    if ((pid = fork()) == 0) {
+
+        printf("Spawning process [PID=%d]:", pid);
+        for (int32_t i = 0; argv[i] != NULL; i++) {
+            printf(" %s", argv[i]);
+        }
+        printf("\n");
+
+        for (int i = STDERR_FILENO + 1; i < 256; i++)
+            close(i);
+        if (!SHARE_PROCESS_GROUP)
+            setsid();
+        execvp(argv[0], argv);
+        perror("Failed");
+        exit(EXIT_FAILURE);
+    }
+    return pid;
+}
+
+int spawn_sync(char *const argv[]) {
+    pid_t pid = spawn(argv);
+    int wstatus;
+    waitpid(0, &wstatus, 0);
+    return wstatus;
+}
 
 int main(int argc, char **argv) {
-    printf("Hello world\n");
+    char *args[] = {"kitty", "-e", "nvim", NULL};
+    int exit_status = spawn_sync(args);
+    printf("exit_status: %d\n", exit_status);
     return 0;
 }
