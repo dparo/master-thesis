ad6954734f6644fcc8f78a41eda3915a5e63cd58
Davide Paro <dparo@outlook.it>
2021-11-25 00:00:32 +0100

    Added licenses

--- src/tools/perfprof/proc.c
+++ src/tools/perfprof/proc.c
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2021 Davide Paro
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "proc.h"
 #include <string.h>
 #include <stdint.h>
@@ -131,10 +152,10 @@ static int32_t pool_sync2(ProcPool *pool) {
 
             int exit_status;
             if (proc_terminated(p->pid, &exit_status)) {
-                printf("Process %s [status: %d]: ",
+                printf("Process %d %s [status=%d]: ", p->pid,
                        exit_status == 0 ? "exited correctly" : "failed",
                        exit_status);
-                printf("(cmd");
+                printf("(CMD");
 
                 for (int32_t i = 0; i < PROC_MAX_ARGS - 1 && p->args[i] != NULL;
                      i++) {
