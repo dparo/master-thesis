e48960a467429077e99f3d5409625d47154eb59c
Davide Paro <dparo@outlook.it>
2022-01-18 20:50:15 +0100

    Moved visualization.{c,h} into render.{c,h}. Adde function to render Cptp Instances into VRPLIB file format

--- src/render.c
+++ src/render.c
@@ -20,7 +20,7 @@
  * SOFTWARE.
  */
 
-#include "visualization.h"
+#include "render.h"
 #include "core-utils.h"
 #include <stdio.h>
 
@@ -66,8 +66,8 @@ const char *guess_plot_filext(const char *filepath, const char *filext) {
     return filext;
 }
 
-bool tour_plot(const char *filepath, const Instance *instance, Tour *tour,
-               const char *filext) {
+bool render_tour_image(const char *filepath, const Instance *instance,
+                       Tour *tour, const char *filext) {
 
     // Try to autodetect the filepath
     filext = guess_plot_filext(filepath, filext);
@@ -178,4 +178,69 @@ terminate:
     }
 
     return result;
+}
+
+void render_instance_into_vrplib_file(FILE *fh, const Instance *instance,
+                                      bool dump_profit_section) {
+    const int32_t n = instance->num_customers + 1;
+
+    bool has_name = instance->name && strlen(instance->name) > 0;
+    fprintf(fh, "NAME : %s\n",
+            has_name ? instance->name : "VRP unnamed instance");
+
+    if (instance->comment && strlen(instance->comment) > 0) {
+        fprintf(fh, "COMMENT : %s\n",
+                instance->comment ? instance->comment : "");
+    }
+
+    fprintf(fh, "TYPE : %s\n", "CVRP");
+    fprintf(fh, "DIMENSION : %d\n", instance->num_customers + 1);
+    fprintf(fh, "VEHICLES : %d\n", instance->num_vehicles);
+    fprintf(fh, "CAPACITY : %f\n", instance->vehicle_cap);
+
+    if (!instance->edge_weight) {
+        fprintf(fh, "EDGE_WEIGHT_FORMAT : %s\n", "FUNCTION");
+        fprintf(fh, "EDGE_WEIGHT_TYPE : %s\n", "EUC_2D");
+    } else {
+        fprintf(fh, "EDGE_WEIGHT_FORMAT : %s\n", "UPPER_ROW");
+        fprintf(fh, "EDGE_WEIGHT_TYPE : %s\n", "EXPLICIT");
+    }
+
+    // Generate node coordinate section
+    fprintf(fh, "NODE_COORD_SECTION\n");
+
+    for (int32_t i = 0; i < instance->num_customers + 1; i++) {
+        fprintf(fh, "%d %g %g\n", i + 1, instance->positions[i].x,
+                instance->positions[i].y);
+    }
+
+    // Generate demand section
+    fprintf(fh, "DEMAND_SECTION\n");
+    for (int32_t i = 0; i < instance->num_customers + 1; i++) {
+        fprintf(fh, "%d %f\n", i + 1, instance->demands[i]);
+    }
+
+    if (instance->edge_weight) {
+        // Generate edge weight section
+        for (int32_t i = 0; i < n; i++) {
+            for (int32_t j = i + 1; j < n; j++) {
+                fprintf(fh, "%d %d %.17f\n", i, j,
+                        instance->edge_weight[sxpos(n, i, j)]);
+            }
+        }
+    }
+
+    if (dump_profit_section) {
+        // Generate profit section
+        fprintf(fh, "PROFIT_SECTION\n");
+        for (int32_t i = 0; i < instance->num_customers + 1; i++) {
+            fprintf(fh, "%d %.17g\n", i + 1, instance->profits[i]);
+        }
+    }
+
+    // Generate depot section
+    fprintf(fh, "DEPOT_SECTION\n");
+    fprintf(fh, "%d\n", 1);
+    fprintf(fh, "%d\n", -1);
+    fprintf(fh, "EOF");
 }
