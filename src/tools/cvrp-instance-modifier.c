92027ace910608c3dd30b54ff197f337103e6f25
Davide Paro <dparo@outlook.it>
2022-01-19 13:04:43 +0100

    cvrp-instance-modifier: automatically detect number of vehicles from filename if not present in the VRPLIB file format

--- src/tools/cvrp-instance-modifier.c
+++ src/tools/cvrp-instance-modifier.c
@@ -24,6 +24,7 @@
 #include "core.h"
 #include "parser.h"
 #include "render.h"
+#include "parsing-utils.h"
 
 enum {
     MAX_NUMBER_OF_ERRORS_TO_DISPLAY = 16,
@@ -39,11 +40,13 @@ typedef struct {
 Instance process_instance(const Instance *instance, const AppCtx *ctx) {
     Instance result = instance_copy(instance, true, true);
 
-    result.vehicle_cap = result.vehicle_cap * ctx->cap_scale_factor;
-    result.num_vehicles =
-        (int32_t)(ceil((double)result.num_vehicles / ctx->cap_scale_factor));
-    result.num_vehicles = MAX(1, result.num_vehicles);
+    if (ctx->cap_scale_factor != 1.0) {
+        result.vehicle_cap = result.vehicle_cap * ctx->cap_scale_factor;
+        result.num_vehicles = (int32_t)(ceil((double)result.num_vehicles /
+                                             ctx->cap_scale_factor));
+    }
 
+    result.num_vehicles = MAX(1, result.num_vehicles);
     return result;
 }
 
@@ -57,6 +60,10 @@ int main2(const AppCtx *ctx) {
     //        VRPLIB entry is not found we substitute it using the command line
     //        specified num_vehicles
     if (ctx->num_vehicles > 0 && instance.num_vehicles == 0) {
+        if (ctx->num_vehicles == 0) {
+            log_fatal("Could not deduce number of vehicles");
+            return EXIT_FAILURE;
+        }
         instance.num_vehicles = ctx->num_vehicles;
     }
 
@@ -82,6 +89,53 @@ int main2(const AppCtx *ctx) {
     return EXIT_SUCCESS;
 }
 
+int32_t autodetect_num_vehicles_from_filename(const char *input_file) {
+    int32_t num_vehicles = 0;
+    Path basename_buf;
+    char *basename = os_basename(input_file, &basename_buf);
+
+    size_t beg = 0;
+    size_t end = 0;
+
+    for (size_t i = 0; basename[i]; i++) {
+        const char preamble[] = "-k";
+        if (0 == strncmp(&basename[i], preamble, strlen(preamble))) {
+            beg = i + strlen(preamble);
+            break;
+        }
+    }
+
+    if (beg > 0) {
+        for (size_t i = beg; basename[i]; i++) {
+            if (basename[i] == '.' || basename[i] == '-') {
+                end = i;
+                break;
+            }
+        }
+    }
+
+    if (beg > 0 && end > 0) {
+        size_t size = 1 + end - beg;
+        char *num_vehicles_str = malloc(size * sizeof(*num_vehicles_str));
+        strncpy_safe(num_vehicles_str, &basename[beg], size);
+        num_vehicles_str[size - 1] = 0;
+
+        if (!str_to_int32(num_vehicles_str, &num_vehicles)) {
+            log_error(
+                "%s :: str_to_int32 failure to convert to int32 string `%s`",
+                __func__, num_vehicles_str);
+        }
+
+        free(num_vehicles_str);
+    }
+
+    if (num_vehicles == 0) {
+        log_error("%s :: Failed autodetect of num of vehicles", __func__);
+    }
+
+    return num_vehicles;
+}
+
 int main(int argc, char **argv) {
     char *progname = argv[0];
     int exitcode = EXIT_SUCCESS;
@@ -93,7 +147,10 @@ int main(int argc, char **argv) {
         arg_file1("o", NULL, NULL, "output instance file");
 
     struct arg_int *num_vehicles =
-        arg_int1("k", "num-vehicles", NULL, "force number of vehicles");
+        arg_int0("k", "num-vehicles", NULL,
+                 "Specify number of vehicles. Default 0: autodetect from "
+                 "filename. Note that the number of vehicles inside the VRPLIB "
+                 "file if present, still takes precedence");
 
     struct arg_dbl *cap_scale_factor = arg_dbl0(
         "f", "cap-scale-factor", NULL, "scale factor for the vehicle capacity");
@@ -111,6 +168,7 @@ int main(int argc, char **argv) {
     }
 
     cap_scale_factor->dval[0] = 1.0;
+    num_vehicles->ival[0] = 0;
 
     {
         int nerrors = arg_parse(argc, argv, argtable);
@@ -129,6 +187,11 @@ int main(int argc, char **argv) {
             exitcode = 1;
             goto exit;
         }
+    }
+
+    if (num_vehicles->ival[0] == 0) {
+        num_vehicles->ival[0] =
+            autodetect_num_vehicles_from_filename(input->filename[0]);
     }
 
     AppCtx ctx = {.input = input->filename[0],
