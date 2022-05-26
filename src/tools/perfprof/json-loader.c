39fd1985328d6728bf096a4a0773a8ad02f33cc8
Davide Paro <dparo@outlook.it>
2022-05-26 09:47:18 +0200

    Fixed some stuff.

--- src/tools/perfprof/json-loader.c
+++ src/tools/perfprof/json-loader.c
@@ -1,22 +1,23 @@
 /*
  * Copyright (c) 2022 Davide Paro
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include "json-loader.h"
@@ -92,6 +93,8 @@ void parse_cptp_solver_json_dump(PerfProfRun *run, cJSON *root) {
     if (BOOL(status & SOLVE_STATUS_CLOSED_PROBLEM) &&
         !BOOL(status & SOLVE_STATUS_PRIMAL_SOLUTION_AVAIL)) {
         primal_bound = INFEASIBLE_SOLUTION_DEFAULT_COST_VAL;
+    } else if (!BOOL(status & SOLVE_STATUS_CLOSED_PROBLEM)) {
+        primal_bound = CRASHED_SOLVER_DEFAULT_COST_VAL;
     } else if (status == SOLVE_STATUS_NULL || BOOL(status & SOLVE_STATUS_ERR)) {
         primal_bound = CRASHED_SOLVER_DEFAULT_COST_VAL;
     }
